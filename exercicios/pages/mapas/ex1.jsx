b77d46a99621e3fe65ac18bcf8eb7279a0790747
RhuaYuri <61441972+RhuaYuri@users.noreply.github.com>
2022-01-26 00:55:11 -0300

    exercicios

---
+++ exercicios/pages/mapas/ex1.jsx
@@ -0,0 +1,61 @@
+import Style from "./css/css.module.css"
+export default function ex1(){
+    return (
+        <div className={Style.estilo}>
+            <div className={Style.chamadaExercicio}>Questão 1</div>
+<p></p>
+<p style={{textAlign: "justify"}}><strong>Questão 1 -</strong> Um veículo trafega em uma rodovia com velocidade média de 80 km/h. Sabendo que a viagem teve uma duração de 1 hora e 30 minutos (1,5 h), qual foi a distância percorrida pelo veículo?</p>
+<table>
+    <tbody>
+        <tr className={Style.tr}>
+            <td valign="top" width="70" className={Style.td}>
+                <label for="obj_3059_0">A)</label> 
+                <input type="radio" id="letraA1" name="fav_language" value="A"/>
+            </td>
+        </tr>
+        <tr className={Style.tr}>
+            <td valign="top" width="70" className={Style.td}>
+                <label for="obj_3059_0">B)</label> 
+                <input type="radio" id="letraB1" name="fav_language" value="B" />
+            </td>
+            <td className={Style.td}>
+            <label for="obj_3059_0">
+                <p style={{textAlign: "justify"}}>vinculação com a filosofia como saber unificado.</p>
+            </label> 
+            </td>
+        </tr>
+        <tr className={Style.tr}>
+            <td valign="top" width="70" className={Style.td}>
+                <label for="obj_3059_0">C)</label> 
+                <input type="radio" id="letraC1" name="fav_language" value="C"/>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<input type="radio" id="html" name="fav_language" value="HTML" />
+<label for="html">HTML</label><br/>
+<input type="radio" id="css" name="fav_language" value="CSS" />
+<label for="css">CSS</label><br/>
+<input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+<label for="javascript">JavaScript</label>
+<p style={{textAlign: "justify"}}>a) 80 km</p>
+
+<p style={{textAlign: "justify"}}>b) 10 km</p>
+
+<p style={{textAlign: "justify"}}>c) 120 km</p>
+
+<p style={{textAlign: "justify"}}>d) 160 km</p>
+
+<p style={{textAlign: "justify"}}>e) 100 km</p>
+
+                                                <p></p>
+
+                                                                                                                                        <div class="titulo-chamada" style={{marginTop: "15px"}}>Questão 2</div>
+                                 
+<a id="mais_questoes" href="https://exercicios.brasilescola.uol.com.br/exercicios-fisica/exercicios-sobre-cinematica.htm" target="_blank">Mais Questões</a>
+ </div>
+    )
+}
+
+
