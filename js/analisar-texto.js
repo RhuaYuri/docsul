80a0939af6878bfb6357e6bd5f2deb4b84ab7f3c
RhuaYuri <61441972+RhuaYuri@users.noreply.github.com>
2022-09-08 01:08:31 -0300

    audio + comeco verificao das atividades

---
+++ js/analisar-texto.js
@@ -0,0 +1,16 @@
+texto = [
+    'Saibão quantos este publico instrumento de escriptura de ven',
+    'da de escravo ou como em direito milhor nome tenha virem,',
+    'que no anno de Nascimento de Nosso Senhor Jesus Christo de mil',
+    'oitocentos setenta e sete, aos trinta dias do mez de Maio do dito',
+    'anno, neta cidade e Comarca da Carolina, da província do Mara',
+    'nhão, em meu escriptorio forão presentes partes justas e contrata',
+    'das a saber: de uma como vendedor o Capitão Baptista'
+]
+
+function analisarTexto(){
+    var e = document.getElementById("line6")
+    console.log(e.value)
+    e.value = "foo"
+
+}
