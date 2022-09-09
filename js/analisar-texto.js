6b384bb7467c479889e9401c46601809faab961a
RhuaYuri <61441972+RhuaYuri@users.noreply.github.com>
2022-09-08 23:37:45 -0300

    atv

--- js/analisar-texto.js
+++ js/analisar-texto.js
@@ -1,6 +1,6 @@
 texto = [
     'instrumento de escriptura de ven', //6
-    'da de escravo ou como em direito milhor nome tenha virem,', //7
+    'da de escravo ou como em direito milhor nome tenha virem', //7
     'que no anno de Nascimento de Nosso Senhor Jesus Christo de mil', //8
     'oitocentos setenta e sete, aos trinta dias do mez de Maio do dito', //9
     'anno, neta cidade e Comarca da Carolina, da província do Mara', //10
@@ -9,15 +9,15 @@ texto = [
     'te nesta cidade, ambos do primeiro distrito e de mim reconhe',//15
     'cidos do que dou fé',//16
     'um escravo de nome Francisco, cabra',//19
-    'com idade hoje de trinta e quatro annos, solteiro, vaqueiro,',//20
-    'natural desta Provincia,',//21
+    'com idade hoje de trinta e quatro annos, solteiro, vaqueiro',//20
+    'natural desta Provincia',//21
     'Joaquim Antonio Nogueira, Collector das rendas geraes',//24
-    'desta Cidade – A requerimento verbal do Capitão João',//25
+    'desta Cidade - A requerimento verbal do Capitão João',//25
     'Baptista Mascarenhas, certifico que revendo o livro de ma',//26
     'do livro, verifiquei achar-se matiulado sob os números',//28
     'quartorze da matricula e trez da relação apresentada em',//29
     'dezenove de Junho de mil oitocentos setenta e dois, perten',//30
-    'de, n`aquelle tempo, solteiro, filho de Maria, vaqueiro.'//32
+    'de, n`aquelle tempo, solteiro, filho de Maria, vaqueiro'//32
 ]
 
 ids = [
@@ -68,11 +68,18 @@ function analisarTexto(){
     var string 
     for(var i = 0; i < texto.length; i++ ){
         string = document.getElementById(ids[i])
-        if (string.value === texto[i]){
-            console.log(string.value)
-            reDiv = document.getElementById(re[i])
-            reDiv.innerHTML = '<i class="gg-check"></i>'
-        }
+        reDiv = document.getElementById(re[i])        
+        try {
+            if (string.value.replace(/^\s+|\s+$/g, '') === texto[i]){
+                reDiv.innerHTML = '<i style="background-color: rgb(5, 218, 33);" class="gg-check"></i>'
+            }else{
+                reDiv.innerHTML = '<i style="background-color: red;" class="bi bi-x"></i>'
+    
+            }
+         }
+         catch (e) {
+            reDiv.innerHTML = '<i style="background-color: red;" class="bi bi-x"></i>'
+         }
     }
 
 }
