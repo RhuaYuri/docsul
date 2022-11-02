ac1a0b6302d22a948b36fe8a1950680073f689d5
RhuaYuri <61441972+RhuaYuri@users.noreply.github.com>
2022-11-02 00:37:13 -0300

    teste t

---
+++ t/main.js
@@ -0,0 +1,35 @@
+var txtInput = document.querySelector('#txtInput')
+var voiceList = document.querySelector('#voiceList')
+var btnSpeak = document.querySelector('#btnSpeak')
+var synth = window.speechSynthesis;
+var voices = []
+
+newVoices()
+if(speechSynthesis !== undefined){
+    speechSynthesis.envoiceschanged = newVoices
+}
+
+btnSpeak.addEventListener('click', () => {
+    var toSpeak = new SpeechSynthesisUtterance(txtInput.value)
+    var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+    voices.forEach((voice) => {
+        if(voice.name == selectedVoiceName){
+            toSpeak.voice = voice
+        }
+    })
+    synth.speak(toSpeak)
+})
+
+function newVoices(){
+    voices = synth.getVoices()
+    var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex
+    voiceList.innerHTML = ''
+    voices.forEach((voice) => {
+        var listItem = document.createElement('option')
+        listItem.setAttribute('data-lang', voice.lang)
+        listItem.setAttribute('data-name', voice.name)
+        voiceList.appendChild(listItem)
+    })
+
+    voiceList.selectedIndex(listItem)
+}
