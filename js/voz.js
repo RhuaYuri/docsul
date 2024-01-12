d90cb7766943757bff5ae21b37b64f7edbd8e111
Rhuã Yuri <61441972+RhuaYuri@users.noreply.github.com>
2024-01-12 01:16:09 -0300

    api audio

--- js/voz.js
+++ js/voz.js
@@ -1,28 +1,65 @@
+var selecaoAtivada = false;
 
-let leituraAtiva = false; // Variável para controlar a ativação da leitura
+function destacarAoPassarMouse(event) {
+    if (selecaoAtivada) {
+        var elementoClicado = event.target;
 
-        function ativarLeitura() {
-            leituraAtiva = true;
+        // Remover destaque dos elementos anteriores
+        removeHighlight();
+
+        // Destacar o elemento ao passar o mouse
+        elementoClicado.classList.add("highlight");
+    }
+}
+
+function removerDestaqueAoSairMouse() {
+    if (selecaoAtivada) {
+        // Remover destaque ao sair do elemento
+        removeHighlight();
+    }
+}
+
+function selecionarTexto(event) {
+    if (selecaoAtivada) {
+        var targetElement = event.target;
+        var range = document.createRange();
+        range.selectNode(targetElement);
+        window.getSelection().removeAllRanges(); // Limpa a seleção anterior
+        window.getSelection().addRange(range); // Adiciona a nova seleção
+        document.execCommand('copy'); // Copia o texto selecionado para a área de transferência
+        window.getSelection().removeAllRanges(); // Limpa a seleção após a cópia (opcional)
+        responsiveVoice.speak(targetElement.textContent, 'Brazilian Portuguese Female', {rate:1})
+        console.log(targetElement.textContent);
+        
+        // Adicione aqui a lógica para falar o texto, no seu caso, o código do responsiveVoice.speak
+    }
+}
+
+function removeHighlight() {
+    var elementosDestacados = document.querySelectorAll(".highlight");
+    elementosDestacados.forEach(function (elementoDestacado) {
+        elementoDestacado.classList.remove("highlight");
+    });
+}
+
+function alternarSelecao() {
+    selecaoAtivada = !selecaoAtivada;
+    var botao = document.getElementById("myBtn");
+    if (selecaoAtivada) {
+        botao.textContent = "Desativar";
+    } else {
+        botao.innerHTML = '<img src="./img/icone_acessibilidade/descricao-de-audio.svg" style="width: 30px !important" alt="" srcset="">'; // Remove o texto do botão
+    }
+    var elementos = document.querySelectorAll("*");
+    elementos.forEach(function (elemento) {
+        if (selecaoAtivada) {
+            elemento.addEventListener("mouseover", destacarAoPassarMouse);
+            elemento.addEventListener("mouseout", removerDestaqueAoSairMouse);
+            elemento.addEventListener("click", selecionarTexto);
+        } else {
+            elemento.removeEventListener("mouseover", destacarAoPassarMouse);
+            elemento.removeEventListener("mouseout", removerDestaqueAoSairMouse);
+            elemento.removeEventListener("click", selecionarTexto);
         }
-
-        document.getElementById("textoParaLeitura").addEventListener("click", function() {
-            if (leituraAtiva) {
-                var textoParaLeitura = document.getElementById("textoParaLeitura");
-
-                // Seleciona o texto para leitura
-                var texto = textoParaLeitura.textContent;
-
-                // Configura a voz e inicia a síntese usando ResponsiveVoice
-                responsiveVoice.speak(texto, "Brazilian Portuguese Female");
-
-                // Destacar o texto selecionado
-                textoParaLeitura.classList.add("selecionado");
-
-                // Remover o destaque após um curto período
-                setTimeout(function() {
-                    textoParaLeitura.classList.remove("selecionado");
-                }, 100); // 1000 milissegundos = 1 segundo
-
-                leituraAtiva = false; // Desativa a leitura após a primeira execução
-            }
-        });
+    });
+}
